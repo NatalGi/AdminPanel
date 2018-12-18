7cb63b23be1abe21213ee45e35f0ad522bacaa7c
Natalia Ginalska <natalia.ginalska@gmail.com>
2018-12-18 11:15:49 +0100

    Add auto-hide function to the sidebar

--- js/script.js
+++ js/script.js
@@ -5,49 +5,77 @@
 "use strict";
 
 (function() {
-        $('#sidebar-toggle-btn').click(function() {
-        var sidebarElement = $('#sidebar');
-        var margin = sidebarElement.css('margin-left');
-        var mainPageSection = $('#main-section');
+  var sidebarElement = $('#sidebar');
+  var mainPageSection = $('#main-section');
+  var windowWidth = $(window).width();
 
-        //console.log('margin: ' + margin);
-        //console.log(sidebarElement.classList);
-        if(!margin || margin == '0px') { //Hide sidebar
-            sidebarElement.addClass('sidebar-hide');
-            sidebarElement.removeClass('sidebar-show');
-            sidebarElement.css({
-              marginLeft: '-141px',
-              overflow: 'hidden'
-            });
-            mainPageSection.addClass('sidebar-hid');
-            mainPageSection.css('padding-left', '46px');
-        }
-        else { //Show sidebar
-            sidebarElement.removeClass('sidebar-hide');
-            sidebarElement.addClass('sidebar-show');
-            sidebarElement.css({
-              marginLeft: '0px',
-              overflow: 'auto'
-            });
-            mainPageSection.addClass('sidebar-showed');
-            mainPageSection.css('padding-left', '187px');
-        }
+  if(windowWidth < 560) {
+    hideSidebar();
+  }
+
+  function showSidebar(fullWidth = false) {
+    sidebarElement.removeClass('sidebar-hide');
+    sidebarElement.addClass('sidebar-show');
+    sidebarElement.css({
+      marginLeft: '0px',
+      overflow: 'auto'
     });
 
-    $(function() {
-        $('#component-select-1 .select').click(function() {
-            $('#component-select-1 .option-list').addClass('transition');
-            $('#component-select-1 .option-list').toggleClass('expand');
-        });
+    if(!fullWidth) {
+      mainPageSection.addClass('sidebar-showed');
+      mainPageSection.css('padding-left', '187px');
+    }
+  }
 
-        $(window).resize(function() {
-            $('#component-select-1 .option-list').removeClass('transition');
-        });
+  function hideSidebar() {
+    sidebarElement.addClass('sidebar-hide');
+    sidebarElement.removeClass('sidebar-show');
+    sidebarElement.css({
+      marginLeft: '-141px',
+      overflow: 'hidden'
+    });
+    mainPageSection.addClass('sidebar-hid');
+    mainPageSection.css('padding-left', '46px');
+  }
 
-        $('#component-select-1 .option-list .option').click(function(event) {
-            $('#component-select-1 .choosen-option').html($(event.target).html());
-        });
-    })
+  //if(windowWidth)
+
+  $('#sidebar-toggle-btn').click(function() {
+    var margin = sidebarElement.css('margin-left');
+    windowWidth = $(window).width();
+
+    //console.log('margin: ' + margin);
+    //console.log(sidebarElement.classList);
+    if(!margin || margin == '0px') { //Hide sidebar
+      hideSidebar();
+    }
+    else { //Show sidebar
+      if(windowWidth < 560) {
+        showSidebar(true);
+      } else {
+        showSidebar();
+      }
+    }
+  });
+
+  $(function() {
+    $('#component-select-1 .select').click(function() {
+      $('#component-select-1 .option-list').addClass('transition');
+      $('#component-select-1 .option-list').toggleClass('expand');
+    });
+
+    $(window).resize(function() {
+      $('#component-select-1 .option-list').removeClass('transition');
+      windowWidth = $(window).width();
+      if(windowWidth < 560) {
+        hideSidebar();
+      }
+    });
+
+    $('#component-select-1 .option-list .option').click(function(event) {
+      $('#component-select-1 .choosen-option').html($(event.target).html());
+    });
+  })
 
     /*document.getElementById('component--select .select').addEventListener('click', function() {
         document.getElementById('option-list').classList.toggle('show');
